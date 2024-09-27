4cc7da7b0d63ea362651c68b3c541e62f419cc17
Ian Rees <ian@ianrees.net>
2024-09-26 23:27:33 -0700

    Add tzcache

---
+++ cache/tzcache/tzcache_test.go
@@ -0,0 +1,43 @@
+package tzcache
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func BenchmarkLookupCache_LoadLocation(b *testing.B) {
+	t := "America/Los_Angeles"
+	time.LoadLocation(t)
+	for n := 0; n < b.N; n++ {
+		loc, err := time.LoadLocation(t)
+		_ = loc
+		_ = err
+	}
+}
+
+func BenchmarkLookupCache_LoadLocationCache(b *testing.B) {
+	lock := sync.Mutex{}
+	c := map[string]*time.Location{}
+	t := "America/Los_Angeles"
+	time.LoadLocation(t)
+	for n := 0; n < b.N; n++ {
+		lock.Lock()
+		if _, ok := c[t]; !ok {
+			loc, err := time.LoadLocation(t)
+			_ = loc
+			_ = err
+			c[t] = loc
+		}
+		lock.Unlock()
+	}
+}
+
+func Benchmark_TzCache(b *testing.B) {
+	c := NewTzCache[int]()
+	for n := 0; n < b.N; n++ {
+		loc, ok := c.Add(n, "America/Los_Angeles")
+		_ = loc
+		_ = ok
+	}
+}
