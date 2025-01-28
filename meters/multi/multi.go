e729feee4acb69456a25fb4695c2abcac64942b7
Ian Rees <ian@ianrees.net>
2025-01-27 17:25:45 -0800

    Update name

--- meters/multi/multi.go
+++ meters/multi/multi.go
@@ -7,24 +7,24 @@ import (
 )
 
 func init() {
-	var _ meters.MeterProvider = &MultiMeter{}
+	var _ meters.MeterProvider = &MultiMeterProvider{}
 }
 
-type MultiMeter struct {
+type MultiMeterProvider struct {
 	meters []meters.MeterProvider
 }
 
-func (m *MultiMeter) NewMeter(user meters.MeterUser) meters.ApiMeter {
+func (m *MultiMeterProvider) NewMeter(user meters.MeterUser) meters.ApiMeter {
 	var mets []meters.ApiMeter
 	for _, m := range m.meters {
 		mets = append(mets, m.NewMeter(user))
 	}
-	return &multiUserMeter{
+	return &multiMeterUser{
 		mets: mets,
 	}
 }
 
-func (m *MultiMeter) Flush() error {
+func (m *MultiMeterProvider) Flush() error {
 	for _, m := range m.meters {
 		if err := m.Flush(); err != nil {
 			return err
@@ -33,7 +33,7 @@ func (m *MultiMeter) Flush() error {
 	return nil
 }
 
-func (m *MultiMeter) Close() error {
+func (m *MultiMeterProvider) Close() error {
 	for _, m := range m.meters {
 		if err := m.Close(); err != nil {
 			return err
@@ -42,13 +42,13 @@ func (m *MultiMeter) Close() error {
 	return nil
 }
 
-func (m *MultiMeter) AddDimension(meterName string, key string, value string) {
+func (m *MultiMeterProvider) AddDimension(meterName string, key string, value string) {
 	// for _, m := range m.meters {
 	// 	m.AddDimension(meterName, key, value)
 	// }
 }
 
-func (m *MultiMeter) GetValue(u meters.MeterUser, meterName string, startTime time.Time, endTime time.Time, checkDims meters.Dimensions) (float64, bool) {
+func (m *MultiMeterProvider) GetValue(u meters.MeterUser, meterName string, startTime time.Time, endTime time.Time, checkDims meters.Dimensions) (float64, bool) {
 	for _, m := range m.meters {
 		if val, ok := m.GetValue(u, meterName, startTime, endTime, checkDims); ok {
 			return val, ok
@@ -57,11 +57,11 @@ func (m *MultiMeter) GetValue(u meters.MeterUser, meterName string, startTime ti
 	return 0, false
 }
 
-type multiUserMeter struct {
+type multiMeterUser struct {
 	mets []meters.ApiMeter
 }
 
-func (m *multiUserMeter) Meter(meterName string, value float64, extraDimensions meters.Dimensions) error {
+func (m *multiMeterUser) Meter(meterName string, value float64, extraDimensions meters.Dimensions) error {
 	for _, m := range m.mets {
 		if err := m.Meter(meterName, value, extraDimensions); err != nil {
 			return err
@@ -70,13 +70,13 @@ func (m *multiUserMeter) Meter(meterName string, value float64, extraDimensions 
 	return nil
 }
 
-func (m *multiUserMeter) AddDimension(meterName string, key string, value string) {
+func (m *multiMeterUser) AddDimension(meterName string, key string, value string) {
 	for _, m := range m.mets {
 		m.AddDimension(meterName, key, value)
 	}
 }
 
-func (m *multiUserMeter) GetValue(meterName string, startTime time.Time, endTime time.Time, dims meters.Dimensions) (float64, bool) {
+func (m *multiMeterUser) GetValue(meterName string, startTime time.Time, endTime time.Time, dims meters.Dimensions) (float64, bool) {
 	for _, m := range m.mets {
 		if val, ok := m.GetValue(meterName, startTime, endTime, dims); ok {
 			return val, ok
