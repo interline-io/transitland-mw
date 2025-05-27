bea1098b36adf51051e30e19ff30dbf7e0a7a7e3
Ian Rees <ian@ianrees.net>
2025-05-27 15:53:30 -0700

    Use os.OpenRoot

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/interline-io/transitland-mw
 
-go 1.23.0
+go 1.24.2
 
 require (
 	github.com/Masterminds/squirrel v1.5.4
--- testutil/http.go
+++ testutil/http.go
@@ -1,18 +1,28 @@
 package testutil
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"path/filepath"
 )
 
 func NewTestServer(baseDir string) *httptest.Server {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		p := r.URL.Path
-		buf, err := os.ReadFile(filepath.Join(baseDir, p))
+		rootDir, err := os.OpenRoot(baseDir)
+		if err != nil {
+			http.Error(w, "internal server error", 500)
+			return
+		}
+		defer rootDir.Close()
+		f, err := rootDir.Open(r.URL.Path)
 		if err != nil {
 			http.Error(w, "not found", 404)
+			return
+		}
+		buf, err := io.ReadAll(f)
+		if err != nil {
+			http.Error(w, "internal server error", 500)
 			return
 		}
 		w.Write(buf)
